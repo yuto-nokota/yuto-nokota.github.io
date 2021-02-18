70c50eff0d79513f66135422b05bb3f5ea589806
yuto-nokota <yuto-nokota@centos8.hyper-v.thinkpad.localdomain>
2021-02-19 04:03:29 +0900

    Add pattern-highlighter

---
+++ pattern-highlighter/main.js
@@ -0,0 +1,40 @@
+let style = document.createElement("style");
+document.head.appendChild(style);
+window.onload= function () {
+  let input  = document.getElementById("input");
+  let result = document.getElementById("result");
+  let css = style.sheet;
+  for ( key in regexps ) {
+    css.insertRule("." + key 
+      + "{background-color:" + regexps[key].bgcolor
+      + ";color:" + regexps[key].fgcolor
+      + ";}",0
+    );
+  }
+  let realtimeOnChange = function () {
+    var str = input.value.replace(/\n/g,"<br>");
+    for ( key in regexps ) {
+      var match = str.match(new RegExp(regexps[key].pattern,"g"));
+      for ( i in match ) {
+        var substr = match[i];
+        if ( !substr.match(new RegExp(excepts.pattern)) ) {
+          str = str.replace(""+substr
+            , "<span class='balloon' class='" + key + "'>" + substr
+            + "<span class='discription'>" + key + "</span>"
+            + "</span>"
+          );
+        }
+      }
+    }
+    input.setAttribute("rows",Math.max(20,(input.value+"\n").match(/\n/g).length+2));
+    result.innerHTML = str;
+  }
+  let pre="",cur="";
+  let id = window.setInterval(function(){
+    cur = document.getElementById("input").value;
+    if ( pre !== cur ) realtimeOnChange();
+    pre = cur;
+  },42);
+}
+
+
