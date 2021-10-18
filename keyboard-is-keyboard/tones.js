d66de645665a083feea278f416fe73a7fef2f810
yuto-nokota <yuto.nokota.ks@gmail.com>
2021-10-18 22:01:26 +0900

    Add exp tone in keyboard-is-keyboard

--- keyboard-is-keyboard/tones.js
+++ keyboard-is-keyboard/tones.js
@@ -1,13 +1,13 @@
+const delta = {
+  'MainFrequencyIndex' : 1,
+  'reals' : [0,1],
+  'imags' : [0,1]
+};
+
 const gaussian = {
   'MainFrequencyIndex' : 16,
   'reals' : Array(32).fill().map((_,i)=>i==0?0:Math.exp(-(i-16)*(i-16)/32)),
   'imags' : Array(32).fill().map((_,i)=>i==0?0:Math.exp(-(i-16)*(i-16)/32))
-};
-
-const sine = {
-  'MainFrequencyIndex' : 1,
-  'reals' : Array(2).fill().map((_,i)=>i==0?0:1),
-  'imags' : Array(2).fill().map((_,i)=>i==0?0:1)
 };
 
 const pink = {
@@ -16,4 +16,9 @@ const pink = {
   'imags' : Array(256).fill().map((_,i)=>i==0?0:1/i)
 };
 
+const exp = {
+  'MainFrequencyIndex' : 1,
+  'reals' : Array(32).fill().map((_,i)=>i==0?0:Math.exp(-(i-1))),
+  'imags' : Array(32).fill().map((_,i)=>i==0?0:Math.exp(-(i-1)))
+};
 
