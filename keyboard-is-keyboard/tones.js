944b556f3f998f00cf2e44c2a8f6fbc311e84a20
yuto-nokota <yuto.nokota.ks@gmail.com>
2021-10-18 21:35:43 +0900

    Fix typo and Fix gaussian tone

--- keyboard-is-keyboard/tones.js
+++ keyboard-is-keyboard/tones.js
@@ -1,7 +1,7 @@
 const gaussian = {
-  'MainFrequencyIndex' : 128,
-  'reals' : Array(256).fill().map((_,i)=>i==0?0:Math.exp(-(i-128)*(i-128)/3640)),
-  'imags' : Array(256).fill().map((_,i)=>i==0?0:Math.exp(-(i-128)*(i-128)/3640))
+  'MainFrequencyIndex' : 16,
+  'reals' : Array(32).fill().map((_,i)=>i==0?0:Math.exp(-(i-16)*(i-16)/32)),
+  'imags' : Array(32).fill().map((_,i)=>i==0?0:Math.exp(-(i-16)*(i-16)/32))
 };
 
 const sine = {
