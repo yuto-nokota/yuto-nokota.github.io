886a3280b27dce512e08e959c35c7b7d2822ee7c
yuto-nokota <yuto.nokota.ks@gmail.com>
2021-10-21 22:15:18 +0900

    Add answers in frequency-magicnumber-quiz

--- frequency-magicnumber-quiz/answers.js
+++ frequency-magicnumber-quiz/answers.js
@@ -1,6 +1,14 @@
 const answers = [
-  { freq: 3776, text:"富士山の標高[m]"},
-  { freq:  997, text:"水の密度(4度)[kg/m3]"},
+  { freq:  103, text:"通天閣の高さ[m]"},
+  { freq:  108, text:"煩悩の数"},
+  { freq:  131, text:"京都タワーの高さ[m]"},
+  { freq:  333, text:"東京タワーの高さ[m]"},
   { freq:  634, text:"スカイツリーの高さ[m]"},
-]
+  { freq:  997, text:"水の密度(4度)[kg/m3]"},
+  { freq: 1221, text:"承久の乱[年]"},
+  { freq: 1333, text:"鎌倉幕府滅亡[年]"},
+  { freq: 3600, text:"1時間の秒数[s]"},
+  { freq: 3776, text:"富士山の標高[m]"},
+  { freq: 8611, text:"K2の標高[m]"},
+];
 
