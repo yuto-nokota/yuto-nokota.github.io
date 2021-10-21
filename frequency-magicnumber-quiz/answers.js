5e981ffd71791f617011f629c4a28be3bd3fe03f
yuto-nokota <yuto.nokota.ks@gmail.com>
2021-10-21 21:30:36 +0900

    Add frequency-magicnumber-quiz

---
+++ frequency-magicnumber-quiz/answers.js
@@ -0,0 +1,6 @@
+const answers = [
+  { freq: 3776, text:"富士山の標高[m]"},
+  { freq:  997, text:"水の密度(4度)[kg/m3]"},
+  { freq:  634, text:"スカイツリーの高さ[m]"},
+]
+
---
+++ frequency-magicnumber-quiz/chronological_table.js
@@ -0,0 +1,3 @@
+const chronological_table = new Array(2000);
+chronological_table[100] = "「説文解字」完成";
+chronological_table[101] = "「説文解字」完成";
