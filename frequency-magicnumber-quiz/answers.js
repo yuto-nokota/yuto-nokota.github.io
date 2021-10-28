97fa95e2f1da2f6c540d6641beec95f0637e585d
yuto-nokota <yuto.nokota.ks@gmail.com>
2021-10-28 17:51:38 +0900

    Add gooogle analytics tag in index page

--- frequency-magicnumber-quiz/answers.js
+++ frequency-magicnumber-quiz/answers.js
@@ -7,6 +7,7 @@ const answers = [
   { freq:  997, text:"水の密度(4度)[kg/m3]"},
   { freq: 1221, text:"承久の乱[年]"},
   { freq: 1333, text:"鎌倉幕府滅亡[年]"},
+  { freq: 2700, text:"お笑いコンビ"},
   { freq: 3600, text:"1時間の秒数[s]"},
   { freq: 3776, text:"富士山の標高[m]"},
   { freq: 8611, text:"K2の標高[m]"},
