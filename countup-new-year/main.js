b7707cb103cb5c780587ff458755082f099165b7
yuto-nokota <yuto.nokota.ks@gmail.com>
2022-01-01 11:05:29 +0900

    Add progress bar

--- countup-new-year/main.js
+++ countup-new-year/main.js
@@ -2,7 +2,12 @@ function show_time() {
   let now  = new Date();
   let year   = now.getFullYear();
   let gantan = new Date(year,0,1,0,0,0,0);
-  document.getElementById('main').innerHTML = (now.getTime() - gantan.getTime()) / 1000;
+  let next   = new Date(year+1,0,1,0,0,0,0);
+  let spend = now.getTime() - gantan.getTime();
+  let progress = Math.floor(spend * 1000 / ( next.getTime() - gantan.getTime() )) / 10;
+  document.getElementById('main').innerHTML = spend / 1000;
+  document.getElementById('progress').innerHTML = 
+    "["+"#".repeat(progress)+"-".repeat(100-Math.floor(progress))+"]" + progress + "%";
 }
 
 function start() {
